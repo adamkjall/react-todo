9ae29e4987afda16f9a5035ed0e735990619b77f
Adam <bottarn.botvidsson@gmail.com>
2019-05-03 09:26:27 +0200

    added a remove button to every entry in the list

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,18 +1,48 @@
 import React from "react";
 
+const liStyle = {
+  width: "60%"
+};
+
+const divStyle = {
+  display: "flex",
+  alignItems: "center"
+};
+
+const buttonStyle = {
+  padding: "10px",
+  margin: "5px",
+  fontSize: "14px",
+  backgroundColor: "rgb(139, 163, 199)",
+  color: "#FFF",
+  border: "none"
+};
+
 class TodoItems extends React.Component {
   createTask = item => {
     return (
-    <li key={item.key}
-        onClick={() => this.props.deleteItem(item.key) }>
-    {item.text}
-    </li>);
-  }
+      <div style={divStyle}>
+        <li style={liStyle} key={item.key}>
+          {item.text}
+        </li>
+        <button
+          style={buttonStyle}
+          onClick={() => this.props.deleteItem(item.key)}
+        >
+          Remove
+        </button>
+      </div>
+    );
+  };
   render() {
     const todoEntries = this.props.entries;
     const listItems = todoEntries.map(this.createTask);
 
-    return <ul className="theList">{listItems}</ul>;
+    return (
+      <ul className="theList" style={divStyle}>
+        {listItems}
+      </ul>
+    );
   }
 }
 
